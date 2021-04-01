e6a636d167e74652efb721a8b0caedef7936ebe3
aherlind <andreshah1@yandex.ru>
2021-04-01 18:54:52 +0300

    add comments to mysql

--- srcs/mysql/src/db_init.sh
+++ srcs/mysql/src/db_init.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+# for initialisation
 rc default
+# setup and start mariadb
 /etc/init.d/mariadb setup
 rc-service mariadb start
+# create and init wordpress database
 mysql -u root mysql < create_db.sql
 mysql -u root wordpress < mysql-service.sql
+# stop mariadb
 rc-service mariadb stop
+# run supervisord
 /usr/bin/supervisord
